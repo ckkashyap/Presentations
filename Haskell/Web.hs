5657b51c502ab6852c6793a0094882b8034d375c
CK Kashyap <ckkashyap@gmail.com>
2012-06-10 19:25:34 +0530

    working version

--- Haskell/Web.hs
+++ Haskell/Web.hs
@@ -1,31 +1,29 @@
 import Control.Monad.State
 
 data Screen = Screen String [HTMLControl] 
-     deriving Show
+     deriving (Show, Eq)
 
-data HTMLText   = HTMLText {htmlTextIdentifier     :: String, htmlTextLabel    :: String} deriving Show
-data HTMLRadio  = HTMLRadio {htmlRadioIdentifier   :: String, htmlRadioAction  :: Action} deriving Show
-data HTMLButton = HTMLButton {htmlButtonIdentifier :: String, htmlButtonText   :: String, htmlButtonAction :: Action} deriving Show
+data HTMLText   = HTMLText {htmlTextIdentifier     :: String, htmlTextLabel    :: String} deriving (Show, Eq)
+data HTMLRadio  = HTMLRadio {htmlRadioIdentifier   :: String, htmlRadioAction  :: Action} deriving (Show, Eq)
+data HTMLButton = HTMLButton {htmlButtonIdentifier :: String, htmlButtonText   :: String, htmlButtonAction :: Action} deriving (Show, Eq)
 
 
 data HTMLControl = C1 HTMLText | C2 HTMLRadio | C3 HTMLButton
-     deriving Show
-
-
+     deriving (Show, Eq)
 
 data Action = 
        None 
      | UpdateText HTMLText String
      | Goto Screen
      | ToggleRadio HTMLRadio
-     deriving Show      
+     deriving (Show, Eq)
 
 type HTML = String
 
-screen2HTML :: String -> Screen -> HTML
-screen2HTML  _ (Screen identifier' []) = ""
-screen2HTML identifier (Screen identifier' cs) = pre ++ body ++ post where
-            body = concat $ map (control2html identifier) cs
+screen2HTML :: Screen -> HTML
+screen2HTML (Screen identifier' []) = ""
+screen2HTML (Screen identifier' cs) = pre ++ body ++ post where
+            body = concat $ map control2html cs
             pre = "<html>\n"
             post = "</html>\n"
 
@@ -38,10 +36,10 @@ action2js identifier (UpdateText ht str) = "<script>function f_" ++ identifier +
 identifiableDiv :: String -> String -> String
 identifiableDiv i str =  "<div id=\"div_" ++ i ++ "\">" ++ str ++ "</div>\n"
 
-control2html :: String -> HTMLControl -> HTML
-control2html identifier (C1 (HTMLText i l)) = identifiableDiv identifier l
-control2html identifier (C2 (HTMLRadio i a)) = (identifiableDiv identifier  "<input id=\"input_" ++ identifier ++ "type=\"radio\"/>") ++ (action2js identifier a)
-control2html identifier (C3 (HTMLButton i l a)) = identifiableDiv identifier ("<input id=\"button_" ++ identifier ++ "type=\"button\"/>")
+control2html :: HTMLControl -> HTML
+control2html (C1 (HTMLText i l)) = identifiableDiv i l
+control2html (C2 (HTMLRadio i a)) = (identifiableDiv i "<input id=\"input_" ++ i ++ " type=\"radio\"/>") ++ (action2js i a)
+control2html (C3 (HTMLButton i l a)) = identifiableDiv i ("<input id=\"button_" ++ i ++ "\" type=\"button\" value=\"" ++ l ++ "\"/>")
 
 data TotalState = TotalState {counter :: Int, workFlow :: [Screen]}
      deriving Show
@@ -77,15 +75,36 @@ newScreen = do
 
 add2screen :: Screen -> HTMLControl -> State TotalState Screen
 add2screen (Screen i cs) c = do
-           return (Screen i (cs ++ [c]))
-           
+           if (elem c cs) then return (Screen i cs)
+            else return (Screen i (cs ++ [c]))
+
+add2workflow :: Screen -> MyState
+add2workflow s = do
+             (TotalState c ss) <- get
+             put (TotalState c (s:ss))
+             return 0
 
 someFun :: MyState
 someFun = do
-        x <- newHTMLTextControl "Hello World"
-        x <- newHTMLTextControl "Hello World"
+        t <- newHTMLTextControl "Hello World"
+        t1 <- newHTMLTextControl "Hello World 123"
+        b <- newHTMLButtonControl "Click Me"
+        s <- newScreen
+        s <- add2screen s (C1 t)
+        s <- add2screen s (C1 t)
+        s <- add2screen s (C1 t1)
+        s <- add2screen s (C3 b)
+        add2workflow s
         return 0
 
 initialState = TotalState {counter = 0, workFlow = []}
 
 (a, s)    = runState someFun initialState
+
+wf = workFlow s
+
+html = screen2HTML (wf !! 0)
+
+
+main = do
+     writeFile "out.html" html
